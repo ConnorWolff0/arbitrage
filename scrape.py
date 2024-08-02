b5b17837ffb726c7ba74ec1abe7ee5de913dced2
Connor Wolff <connor.wolff@yale.edu>
2024-08-02 11:43:21 -0400

    1

---
+++ scrape.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+page_to_scrape = requests.get("https://quotes.toscrape.com/")
+soup = BeautifulSoup(page_to_scrape.text, "html.parser")
+
+#Find all quote & author elements 
+quotes = soup.findAll("span", attrs={"class": "text"})
+authors = soup.findAll("small", attrs={"class": "author"})
+
+#Open the CSV file for writing
+with open("scraped_quotes.csv", "w", newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["QUOTES", "AUTHORS"])
+    #Iterate over the paired quotes and authors
+    for quote, author in zip(quotes, authors):
+        #print(type(quote), type(author))
+        print(quote.text + " _ " + author.text)
+        writer.writerow([quote.text, author.text])
